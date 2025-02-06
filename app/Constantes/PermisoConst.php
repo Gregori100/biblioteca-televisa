744679635e5af39139d0eba3e2f87d9393d304b4
DiegoCassellis <diegovelcaz@hotmail.com>
2025-02-05 21:22:49 -0600

    feat(permisos): Agregar permisos en vistas

--- app/Constantes/PermisoConst.php
+++ app/Constantes/PermisoConst.php
@@ -5,10 +5,22 @@ namespace App\Constantes;
 class PermisoConst
 {
   const LISTADO_PERMISOS = [
-    // Lineas
+    // Libros
     'libros.agregar' => 'biblioteca.libros.agregar',
     'libros.editar' => 'biblioteca.libros.editar',
     'libros.eliminar' => 'biblioteca.libros.eliminar',
     // 'libros.ocupar' => 'biblioteca.libros.ocupar',
+
+    // Usuarios
+    'usuarios.agregar' => 'biblioteca.usuarios.agregar',
+    'usuarios.editar' => 'biblioteca.usuarios.editar',
+    'usuarios.eliminar' => 'biblioteca.usuarios.eliminar',
+    'usuarios.editarPassword' => 'biblioteca.usuarios.cambiarContrasena',
+
+    // Perfiles
+    'usuarios.agregarPerfil' => 'biblioteca.perfiles.agregar',
+    'usuarios.eliminarPerfil' => 'biblioteca.perfiles.editar',
+    'usuarios.editarPerfil' => 'biblioteca.perfiles.eliminar',
+    'usuarios.editarPermisosPerfil' => 'biblioteca.perfiles.editarPermisos',
   ];
 }
--- app/Constantes/PermisoEnVistaConst.php
+++ app/Constantes/PermisoEnVistaConst.php
@@ -12,5 +12,31 @@ class PermisoEnVistaConst
       'eliminar' => 'biblioteca.libros.eliminar',
       'ocupar' => 'biblioteca.libros.eliminar',
     ],
+
+    // Usuarios
+    'usuarios.gestor' => [
+      'agregar'  => 'biblioteca.usuarios.agregar',
+      'editar'   => 'biblioteca.usuarios.editar',
+      'eliminar' => 'biblioteca.usuarios.eliminar',
+    ],
+    'usuarios.detalle' => [
+      'editar'         => 'biblioteca.usuarios.editar',
+      'eliminar'       => 'biblioteca.usuarios.eliminar',
+      'editarPassword' => 'biblioteca.usuarios.cambiarContrasena',
+    ],
+
+    // Perfiles
+    'usuarios.perfilesGestor' => [
+      'agregar'  => 'biblioteca.perfiles.agregar',
+      'eliminar' => 'biblioteca.perfiles.eliminar',
+    ],
+    'usuarios.detallePerfil' => [
+      'editar'         => 'biblioteca.perfiles.editar',
+      'eliminar'       => 'biblioteca.perfiles.eliminar',
+      'editarPermisos' => 'biblioteca.perfiles.editarPermisos',
+    ],
+    'usuarios.editarPermisos' => [
+      'editarPermisos' => 'biblioteca.perfiles.editarPermisos',
+    ],
   ];
 }
