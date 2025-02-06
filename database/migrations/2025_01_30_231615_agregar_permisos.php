8339c9d193371df4c3cb6c3fa59fa64eb7027a11
DiegoCassellis <diegovelcaz@hotmail.com>
2025-02-05 21:22:37 -0600

    feat(migraciones): Agregar permisos para usuarios y perfiles

--- database/migrations/2025_01_30_231615_agregar_permisos.php
+++ database/migrations/2025_01_30_231615_agregar_permisos.php
@@ -46,6 +46,72 @@ return new class extends Migration
         'seccion'       => 'Libros',
         'orden_seccion' => 1,
       ],
+
+      // Perfiles y Usuarios
+      [
+        'permiso_id'    => 5,
+        'codigo'        => 'biblioteca.usuarios.agregar',
+        'titulo'        => 'Alta de usuario.',
+        'descripcion'   => 'Permite dar de alta usuarios.',
+        'seccion'       => 'Usuarios',
+        'orden_seccion' => 2,
+      ],
+      [
+        'permiso_id'    => 6,
+        'codigo'        => 'biblioteca.usuarios.editar',
+        'titulo'        => 'Edición de usuario.',
+        'descripcion'   => 'Permite editar usuarios.',
+        'seccion'       => 'Usuarios',
+        'orden_seccion' => 2,
+      ],
+      [
+        'permiso_id'    => 7,
+        'codigo'        => 'biblioteca.usuarios.eliminar',
+        'titulo'        => 'Eliminación de usuario.',
+        'descripcion'   => 'Permite eliminar usuarios.',
+        'seccion'       => 'Usuarios',
+        'orden_seccion' => 2,
+      ],
+      [
+        'permiso_id'    => 8,
+        'codigo'        => 'biblioteca.usuarios.cambiarContrasena',
+        'titulo'        => 'Edición contraseña de usuario.',
+        'descripcion'   => 'Permite eliminar usuarios.',
+        'seccion'       => 'Usuarios',
+        'orden_seccion' => 2,
+      ],
+      [
+        'permiso_id'    => 9,
+        'codigo'        => 'biblioteca.perfiles.agregar',
+        'titulo'        => 'Alta de perfiles.',
+        'descripcion'   => 'Permite dar de alta perfiles de acceso.',
+        'seccion'       => 'Perfiles',
+        'orden_seccion' => 3,
+      ],
+      [
+        'permiso_id'    => 10,
+        'codigo'        => 'biblioteca.perfiles.editar',
+        'titulo'        => 'Edición de perfiles.',
+        'descripcion'   => 'Permite editar perfiles de acceso.',
+        'seccion'       => 'Perfiles',
+        'orden_seccion' => 3,
+      ],
+      [
+        'permiso_id'    => 11,
+        'codigo'        => 'biblioteca.perfiles.eliminar',
+        'titulo'        => 'Eliminación de perfiles.',
+        'descripcion'   => 'Permite eliminar perfiles de acceso.',
+        'seccion'       => 'Perfiles',
+        'orden_seccion' => 3,
+      ],
+      [
+        'permiso_id'    => 12,
+        'codigo'        => 'biblioteca.perfiles.editarPermisos',
+        'titulo'        => 'Edición permisos de perfil.',
+        'descripcion'   => 'Permite editar los permisos relacionados al perfil.',
+        'seccion'       => 'Perfiles',
+        'orden_seccion' => 3,
+      ],
     ]);
   }
 
@@ -59,6 +125,14 @@ return new class extends Migration
       'biblioteca.libros.editar',
       'biblioteca.libros.eliminar',
       'biblioteca.libros.ocupar',
+      'biblioteca.usuarios.agregar',
+      'biblioteca.usuarios.editar',
+      'biblioteca.usuarios.eliminar',
+      'biblioteca.usuarios.cambiarContrasena',
+      'biblioteca.perfiles.agregar',
+      'biblioteca.perfiles.editar',
+      'biblioteca.perfiles.eliminar',
+      'biblioteca.perfiles.editarPermisos',
     ])->delete();
   }
 };
