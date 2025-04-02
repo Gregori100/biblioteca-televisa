c91c812511f652020f5ce1e8aa7a4a899436df40
DiegoCassellis <diegovelcaz@hotmail.com>
2025-04-01 20:17:15 -0600

    feat(api): Agregar endpoint para crones

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CronController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('cron')->name('cron.')->group(function () {
+  Route::middleware([])->group(function () {
+    Route::controller(CronController::class)->group(function () {
+      Route::post('/testCron/{dryrun}', 'testCron');
+    });
+  });
 });
